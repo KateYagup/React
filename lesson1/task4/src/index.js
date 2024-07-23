104ed4d7eb3f5a6a691fdaa6ec9ec9f1d9ae12df
kate <kata3140@gmail.com>
2024-07-23 14:05:18 +0300

    task4

---
+++ lesson1/task4/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+const rootElement = document.querySelector('#root');
+
+const renderGreeting = elem => {
+  // const containerElem = document.createElement('div');
+  // containerElem.classList.add('greeting');
+
+  // const titleElem = document.createElement('h1');
+  // titleElem.classList.add('greeting__title');
+  // titleElem.textContent = 'Hello, world!';
+
+  // const textElem = document.createElement('p');
+  // textElem.classList.add('greeting__text');
+  // textElem.textContent = 'I am learning React';
+
+  // containerElem.append(titleElem, textElem);
+
+  const containerElem = (
+    <div className='greeting'>
+      <h1 className='greeting__title'>Hello, world!</h1>
+      <p className='greeting__text'>'I am learning React'</p>
+    </div>
+  )
+
+  ReactDOM.render(containerElem, elem);
+  // elem.append(containerElem);
+};
+
+renderGreeting(rootElement);
