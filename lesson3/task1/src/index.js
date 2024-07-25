581ba016be1edd1d226e3b7efea70e1bfffbde5a
kate <kata3140@gmail.com>
2024-07-25 21:06:52 +0300

    new

---
+++ lesson3/task1/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Search from './Search'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const Search = (props) => {
+//   console.log(props);
+//   const element = (
+//     <div className="search">
+//       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//       <div className="search__field">
+//         <input type="text" className="search__input" />
+//         <button className="search__button">Search</button>
+//       </div>
+//     </div>
+//   );
+
+//   return element;
+// }
+
+root.render(
+  <Search name='Bob' />
+
+);
+
+
