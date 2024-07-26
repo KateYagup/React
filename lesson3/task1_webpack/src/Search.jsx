640039d19de498bc6145cde2c08064d6d63e0c13
kate <kata3140@gmail.com>
2024-07-26 12:15:29 +0300

    task2

--- lesson3/task1_webpack/src/Search.jsx
+++ lesson3/task1_webpack/src/Search.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Search = (props) => {
-  console.log(props);
-  const element = (
+const Search = props => {
+  return (
     <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+      <h1 className="search__title">`Hello, {props.name}. What to search for you?`</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>
       </div>
     </div>
   );
-  return element;
 };
+
 export default Search;
