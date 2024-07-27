8d94e27a6eb0f56c8b310b407f16fe6fd436c289
kate <kata3140@gmail.com>
2024-07-27 20:04:15 +0300

    task3

---
+++ lesson3/task3/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Comment from "./Comment";
+
+const userInfo = {
+    name: 'Tom',
+    avatarUrl: 'https://avatars1.githubsercontent.com',
+    //   avatarUrl:
+    //     'https://flomaster.top/o/uploads/posts/2023-11/1700141858_flomaster-top-p-multyashnii-risunok-kozi-narisovannie-temp-3.jpg',
+};
+
+const App = () => {
+    return (
+        <Comment
+            user={userInfo}
+            text="Good job!"
+            date={new Date('2019-01-01T11:32:19.566Z')}
+        />
+    )
+}
+
+export default App;
--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Comment from './Comment';
+import App from './App';
+
 
 const rootElement = document.querySelector('#root');
 
-const userInfo = {
-  name: 'Tom',
-  //   avatarUrl: 'https://avatars1.githubsercontent.com',
-  avatarUrl:
-    'https://flomaster.top/o/uploads/posts/2023-11/1700141858_flomaster-top-p-multyashnii-risunok-kozi-narisovannie-temp-3.jpg',
-};
 
-ReactDOM.render(<Comment user={userInfo} />, rootElement);
+ReactDOM.render(
+  <App />,
+  rootElement,
+);
