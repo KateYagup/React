553ff1b616abdad4396ca05261c3686ba13c53ee
kate <kata3140@gmail.com>
2024-07-29 10:55:38 +0300

    new

--- lesson3/task3/src/App.jsx
+++ lesson3/task3/src/App.jsx
@@ -3,7 +3,7 @@ import Comment from "./Comment";
 
 const userInfo = {
     name: 'Tom',
-    avatarUrl: 'https://avatars1.githubsercontent.com',
+    avatarUrl: 'https://avatars1.githubusercontent.com/u/9919?v=4',
     //   avatarUrl:
     //     'https://flomaster.top/o/uploads/posts/2023-11/1700141858_flomaster-top-p-multyashnii-risunok-kozi-narisovannie-temp-3.jpg',
 };
