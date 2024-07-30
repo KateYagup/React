757ba9ada60564378688c0a0b1f2bbb8d80f0cdd
kate <kata3140@gmail.com>
2024-07-30 09:45:44 +0300

    task4

---
+++ lesson3/task4/src/Greeting.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('DD MMM YYYY');
+// const bithDate = formatDate(props.birthDate);
+// const todayDate = new Date();
+// const todaYear = getYear(todayDate);
+
+const Greeting = (props) => {
+    const birthDate = formatDate(props.birthDate);
+    const todayDate = new Date();
+    // const todaYear = getYear(todayDate);
+    // console.log(birthDate.getFullYear());
+    console.log(props.birthDate.getFullYear());
+    console.log(props.birthDate.getMonth());
+    const age = todayDate.getMonth() - props.birthDate.getMonth() < 0
+        ? todayDate.getFullYear() - props.birthDate.getFullYear() - 1
+        : todayDate.getFullYear() - props.birthDate.getFullYear();
+    console.log(age);
+    return (
+        <div className="greeting">
+            {`My name is ${props.firstName} ${props.lastName}. 
+            I am ${age} years old`
+            }
+            {/* {`My name is ${props.firstName} ${props.lastName}. I am 12 years old`} */}
+        </div>
+    )
+}
+
+export default Greeting;
---
+++ lesson3/task4/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Greeting from './Greeting';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(
+    <Greeting
+        firstName='John'
+        lastName='Doe'
+        birthDate={new Date(1975, 7, 23)}
+    />,
+    rootElement
+);
