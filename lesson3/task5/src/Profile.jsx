4d0b778400bc8561e8029aa02665332d3980fa96
kate <kata3140@gmail.com>
2024-07-31 10:32:06 +0300

    new

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -7,10 +7,10 @@ const Profile = (props) => {
     return (
         <div>
             <div className='profile__name'>
-                {`${props.userData.firstName} ${props.userData.lastName} `}
+                {`${props.userData.firstName} ${props.userData.lastName}`}
             </div>
             <div className='profile__birth'>
-                {`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace} `}
+                {`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
             </div>
         </div>
     )
