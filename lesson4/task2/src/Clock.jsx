8b7c1d481ee737e5511415312eb7b0b46ee2e2c1
kate <kata3140@gmail.com>
2024-08-02 19:05:49 +0300

    new

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Component } from "react";
 import './clock.scss';
+import moment from 'moment';
 
 const getTimeWithOffset = offset => {
     const currentTime = new Date();
@@ -9,26 +10,31 @@ const getTimeWithOffset = offset => {
     return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 };
 
+console.log(getTimeWithOffset(-2));
+
 class Clock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            offset: getTimeWithOffset(this.props.offset),
-            // offset: this.props.offset,
+            offset: this.props.offset,
         }
-        // setInterval(() => {
-        //     this.setState({
-        //         offset: this.state.offset + 1,
-        //     })
-        // }, 1000);
+        setInterval(() => {
+            this.setState({
+                offset: this.state.offset + 1,
+            })
+        }, 1000);
     }
 
     render() {
+        console.log(this.props.offset);
+        console.log(getTimeWithOffset(this.props.offset));
+        const time = getTimeWithOffset(this.props.offset);
+        console.log(time.getDay());
         return (
             <>
                 <div className="clock">
                     <div className="clock__location">{this.props.location}</div>
-                    <div className="clock__time">{this.state.offset}</div>
+                    <div className="clock__time">{moment(time).format('LTS')}</div>
                 </div>
             </>
         )
