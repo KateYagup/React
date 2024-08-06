9a6938f10d5d6eb0d62dd120f52debbf14d30a48
kate <kata3140@gmail.com>
2024-08-06 21:46:43 +0300

    new

--- lesson5/task2/src/GoodButton.jsx
+++ lesson5/task2/src/GoodButton.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 class GoodButton extends React.Component {
-    handleClick(event) {
-        console.log(event.target.textContent);
+    handleClick(e) {
+        console.log(e.target.textContent);
         alert('Click me!');
     }
     render() {
