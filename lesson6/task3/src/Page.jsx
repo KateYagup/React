f4bcbe6862aba23f2df4b7bcd70a5f0312a2d0f5
kate <kata3140@gmail.com>
2024-08-10 19:27:12 +0300

    task3

---
+++ lesson6/task3/src/Page.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Info from "./Info";
+
+const text1 = 'Hello, world!';
+const text2 = 'Another exciting text.';
+
+
+class Page extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: null
+        }
+    }
+
+    setText = text => {
+        this.setState({
+            text
+        })
+    }
+
+    render() {
+        return (
+            <div  >
+                <Info text={this.state.text} />
+                <div className="text">*text here*</div>
+                <div className="actions">
+                    <button className="btn" onClick={() => this.setText(text1)}>IPhone 13</button>
+                    <button className="btn" onClick={() => this.setText(text2)}>IPhone 13 Pro</button>
+                    <button className="btn" onClick={() => this.setText('')}>Clear</button>
+                </div>
+            </div >
+        );
+    }
+}
+
+export default Page;
