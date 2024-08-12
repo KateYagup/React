85239a2a7936ecc18f3ce44cf47a65c68e8a09d2
kate <kata3140@gmail.com>
2024-08-12 17:58:54 +0300

    new

--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -2,31 +2,16 @@ import React, { Component } from "react";
 import Offline from "./Offline";
 import Online from "./Online";
 
-class Status extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOnline: true
-        }
-    }
-
-    setOnline = isOnline => {
-        this.setState({
-            isOnline
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                {
-                    this.state.isOnline ?
-                        < Online />
-                        : <Offline />
-                }
-            </div>
-        );
-    }
+const Status = ({ isOnline }) => {
+    return (
+        <div>
+            {
+                isOnline ?
+                    < Online />
+                    : <Offline />
+            }
+        </div>
+    )
 }
 
 export default Status;
