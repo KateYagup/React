37e2ecb548cde21a68e6ba37fb7a50373c8dd9cf
kate <kata3140@gmail.com>
2024-08-19 18:17:23 +0300

    new

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -2,14 +2,16 @@ import moment from "moment/moment";
 import React from "react";
 // import './moment.js';
 
+const formatter = new Intl.NumberFormat('en-GB');
+
 const Transaction = ({ id, time, from, to, rate, amount }) => {
     return (
         <div>
-            <span className='transaction__date transaction'>{moment(time).format('D MMM')}</span>
+            <span className='transaction__date transaction'>{moment(time).format('DD MMM')}</span>
             <span className='transaction__time transaction'>{moment(time).format('HH:mm ')}</span>
             <span className='transaction__assets transaction' >{from} → {to}</span>
-            <span className='transaction__rate transaction'>{new Intl.NumberFormat('en-GB').format(rate)}</span>
-            <span className='transaction__amount transaction' >{new Intl.NumberFormat('en-GB').format(amount)}</span>
+            <span className='transaction__rate transaction'>{formatter.format(rate)}</span>
+            <span className='transaction__amount transaction' >{formatter.format(amount)}</span>
 
         </div>
     )
