6dae001fe4535c521800d963ff1c1b44639db787
kate <kata3140@gmail.com>
2024-08-22 13:01:23 +0300

    task2

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -1,8 +1,31 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import Clock from './Clock';
 
-const App = () => {
-    return <div>Hello, React!</div>;
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: true,
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            visible: !this.state.visible,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggle}>Toggle</button>
+                <div>{this.state.visible && <Clock />}</div>
+            </div>
+        )
+    };
+}
 
 export default App;
