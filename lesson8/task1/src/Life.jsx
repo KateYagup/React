efb270ba71511283c99552a026fedf3129297fc4
kate <kata3140@gmail.com>
2024-08-24 12:57:44 +0300

    new

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -17,9 +17,7 @@ class Life extends Component {
 
 
     componentDidUpdate(prevProps, prevState) {
-        if (prevState) {
-            console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-        }
+        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
 
     componentWillUnmount() {
