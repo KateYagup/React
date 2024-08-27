22f788fa1d2e300479d7c9e80d19082ecf3a9efe
kate <kata3140@gmail.com>
2024-08-27 13:22:15 +0300

    new

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -2,31 +2,31 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            visible: true,
-        };
-        this.toggle = this.toggle.bind(this);
-    }
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             visible: true,
+//         };
+//         this.toggle = this.toggle.bind(this);
+//     }
 
-    toggle() {
-        this.setState({
-            visible: !this.state.visible,
-        })
-    }
+//     toggle() {
+//         this.setState({
+//             visible: !this.state.visible,
+//         })
+//     }
 
-    render() {
-        return (
-            <div>
-                {/* <button onClick={this.toggle}>Toggle</button> */}
-                <div>{this.state.visible && <Clock location="London" offset={0} />}</div>
-                <div>{this.state.visible && <Clock location="Kyiv" offset={2} />}</div>
-                <div>{this.state.visible && <Clock location="New York" offset={-5} />}</div>
-            </div>
-        )
-    };
+render() {
+    return (
+        <div>
+            {/* <button onClick={this.toggle}>Toggle</button> */}
+            <div>{this.state.visible && <Clock location="London" offset={0} />}</div>
+            <div>{this.state.visible && <Clock location="Kyiv" offset={2} />}</div>
+            <div>{this.state.visible && <Clock location="New York" offset={-5} />}</div>
+        </div>
+    )
+};
 }
 
 export default App;
