b81b63e9c2bfe1da53f451d2f7a39b50b1ecd4f5
kate <kata3140@gmail.com>
2024-08-29 19:47:04 +0300

    new

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -24,11 +24,6 @@ class ConnectionStatus extends Component {
     render() {
         const name = 'status status_offline';
         const name1 = 'status';
-        const showState = <div className={this.state.status == 'offline' ? name : name1}>
-            {this.state.status == 'offline' ? 'offline' : 'online'}
-        </div>
-        // showState.classList.add()
-        console.log(this.state.status);
         return <div className={this.state.status == 'offline' ? name : name1}>
             {this.state.status == 'offline' ? 'offline' : 'online'}
         </div>;
