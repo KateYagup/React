cf61d53954f51c94cb2d14a02b03272241868f17
kate <kata3140@gmail.com>
2024-08-30 11:22:29 +0300

    new

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
-    state = {
-        status: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: 'online',
+        }
     }
 
     componentDidMount() {
