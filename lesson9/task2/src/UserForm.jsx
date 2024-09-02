d9629a5bb0bbd0489b8d5c78240076793c7560f9
kate <kata3140@gmail.com>
2024-09-02 13:15:47 +0300

    new

--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -28,7 +28,8 @@ class UserForm extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        console.log(this.state);
+        this.props.onSubmit(this.state)
+        // console.log(this.state);
     }
     render() {
         return (
