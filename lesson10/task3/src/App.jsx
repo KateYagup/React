56ff4b98c5d95d3124da5c717256fc5bb9ab3f73
kate <kata3140@gmail.com>
2024-09-14 21:51:48 +0300

    task3

---
+++ lesson10/task3/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+// import BoilingVerdict from './BoilingVerdict';
+import Calculator from './Calculator';
+
+
+const App = () => {
+
+    return <>
+        {/* <BoilingVerdict celsius={10} /> */}
+        <Calculator />
+    </>
+};
+
+export default App;
+
